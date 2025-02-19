2e89a8b00d0a1443951658b1da09b2dfc2fff373
asraful-07 <asraful0508@gmail.com>
2025-02-19 11:59:25 +0600

    profile done

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,9 +1,62 @@
-import React from "react";
+import { Helmet } from "react-helmet-async";
+import UseAuth from "../../hooks/UseAuth";
+import useRole from "../../hooks/useRole";
 
 const Profile = () => {
+  const { user, loading } = UseAuth();
+  const [role, isLoading] = useRole();
+  if ((loading, isLoading)) return <h1>loading..........</h1>;
   return (
-    <div>
-      <h1>profile</h1>
+    <div className="flex justify-center items-center h-screen">
+      <Helmet>
+        <title>Profile</title>
+      </Helmet>
+      <div className="bg-white shadow-lg rounded-2xl md:w-4/5 lg:w-3/5">
+        <img
+          alt="cover photo"
+          src="https://images.unsplash.com/photo-1564986021890-6f721f19c210?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D"
+          className="w-full mb-4 rounded-t-lg h-56"
+        />
+        <div className="flex flex-col items-center justify-center p-4 -mt-16">
+          <a href="#" className="relative block">
+            <img
+              alt="profile"
+              src={user?.photoURL}
+              className="mx-auto object-cover rounded-full h-24 w-24  border-2 border-white "
+            />
+          </a>
+
+          <p className="p-2 px-4 text-xs text-white bg-pink-600 rounded-full">
+            {role}
+          </p>
+          <p className="mt-2 text-xl font-medium text-gray-800 ">
+            User Id: {user.uid}
+          </p>
+          <div className="w-full p-2 mt-4 rounded-lg">
+            <div className="flex flex-wrap items-center justify-between text-sm text-gray-600 ">
+              <p className="flex flex-col">
+                Name
+                <span className="font-bold text-black ">
+                  {user.displayName}
+                </span>
+              </p>
+              <p className="flex flex-col">
+                Email
+                <span className="font-bold text-black ">{user.email}</span>
+              </p>
+
+              <div>
+                <button className="bg-pink-600 px-10 py-1 rounded-lg text-white cursor-pointer hover:bg-pink-800 block mb-1">
+                  Update Profile
+                </button>
+                <button className="bg-pink-600 px-7 py-1 rounded-lg text-white cursor-pointer hover:bg-pink-800">
+                  Change Password
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
